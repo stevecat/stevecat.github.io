6e52f870d4956edab334100dc71bd88a53d680eb
Steve Guntrip <stevecat@github.com>
2015-08-16 22:18:03 -0700

    Primer, octagons, design and another example article.
    Using bower for components.

--- articles.js
+++ articles.js
@@ -28,6 +28,9 @@ function show_posts(inc) {
     console.log("No articles.")
   }
 
+  // Update menu links
+  fill_dates();
+
 }
 
 function article_container_html(html, id) {
@@ -38,5 +41,74 @@ function md2html(md) {
 
   var converter = new showdown.Converter();
   return converter.makeHtml(md);
+
+}
+
+function make_date_object(path) {
+
+  // turn a YYYY-MM-DD.md into a useful date.
+
+    // Drop the .md
+    var full = path.substring(0, path.length - 3);
+
+    // Explode by the hyphens
+    var units = full.split("-");
+
+    // Create a JS date
+    var d = new Date(units[0], (units[1]-1), units[2]);
+
+    // Make object with nicely formatted strings.
+    var obj = {};
+
+      // Month Year, for the menu.
+      obj.monthyear = months[d.getMonth()]+" "+units[0];
+
+      // Full date for display
+      obj.nicedate = parseInt(units[2])+nth(units[2])+" "+months[d.getMonth()]+" "+units[0];
+
+    return obj;
+
+}
+
+function nth(d) {
+  if(d>3 && d<21) return 'th'; // thanks kennebec
+  switch (d % 10) {
+        case 1:  return "st";
+        case 2:  return "nd";
+        case 3:  return "rd";
+        default: return "th";
+    }
+}
+
+function fill_dates() {
+
+  // Compile date counts
+  var count = {};
+
+  for (var i = 0; i < filemem.length; i++) {
+
+    var thisdate = filemem[i].date.monthyear;
+
+    if (count[thisdate]) {
+      count[thisdate]++;
+    } else {
+      count[thisdate]=1;
+    }
+
+  }
+
+  // Create some links
+  var html = "";
+
+  $.each(count, function( index, value ) {
+
+    html += "<a class=\"menu-item\" href=\"#\">";
+    html += "<span class=\"counter\">"+value+"</span>";
+    html += index;
+    html += "</a>";
+
+  });
+
+  $('#blog-sub').html(html);
 
 }
